2877c5d46db40c822267e99300586a3d2a68fe52
Jessica Wu <hongwanjwu@gmail.com>
2019-10-10 00:06:15 -0700

    Redesign db schema and create example query to test data retrival

--- Reservation/database/schema.sql
+++ Reservation/database/schema.sql
@@ -1,18 +1,29 @@
-DROP DATABASE IF EXISTS Air6n6; 
-CREATE DATABASE Air6n6;
-USE Air6n6;
+DROP DATABASE IF EXISTS air6n6; 
+CREATE DATABASE air6n6;
+-- USE Air6n6;
 
-CREATE TABLE Listings (
-  listingId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(100) NOT NULL,
-  basePrice INT NOT NULL
-); 
+-- CREATE TABLE Listings (
+--   listingId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--   name VARCHAR(100) NOT NULL,
+--   basePrice INT NOT NULL, 
+--   cleaningFee INT NOT NULL,
+--   serviceFee INT NOT NULL,
+--   minStay,
+--   maxStay,
+--   instantBooked,
+--   maxGuest
+-- ); 
 
-CREATE TABLE Reservations (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  user VARCHAR(100) NOT NULL,
-  checkin DATE NOT NULL,
-  checkout DATE NOT NULL,
-  listingId INT NOT NULL,
-  FOREIGN KEY (listingId) REFERENCES Listings(listingId)
-);
+-- CREATE TABLE Reservations (
+--   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--   user VARCHAR(100) NOT NULL,
+--   month INT,
+--   date INT,
+--   checkin DATE NOT NULL,
+--   total nights,
+--   listingId INT NOT NULL,
+--   children
+--   adults
+--   infants
+--   FOREIGN KEY (listingId) REFERENCES Listings(listingId)
+-- );
--- Reservation/database/seed.js
+++ Reservation/database/seed.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 const config = require('../config.js');
-const schema = require('./schema.js');
+const faker = require('faker');
 
-const db = new Sequelize('', 'root', config.sqlPW || '', {
+const db = new Sequelize('air6n6', 'root', config.sqlPW || '', {
   dialect: 'mysql'
 });
 
@@ -13,3 +13,96 @@ db.authenticate()
   .catch(error => {
     console.errror('unable to connect', error);
   });
+
+const Listing = db.define('listing', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  basePrice: {
+    type: Sequelize.INTERGER,
+    allowNull: false
+  },
+  cleaningFee: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  serviceFee: {
+    type: Sequelize.INTERGER,
+    allowNull: false
+  },
+  minStay: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  maxStay: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  instantBooked: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false
+  },
+  maxGuest: {
+    type: Sequelize.INTERGER
+  }
+});
+
+const Reservation = db.define('reservation', {
+  id: {
+    type: Sequelize.BOOLEAN,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  checkIn: {
+    type: Sequelize.DATE,
+    allowNull: false
+  },
+  totalNights: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  guests: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  }
+});
+
+Listing.hasMany(Reservation, { as: 'Reservations' });
+
+//example query & record creation:
+let createRecords = () => {
+  Listing.sync()
+    .then(() =>
+      Listing.create({
+        name: 'LA'
+      })
+    )
+    .then(() => Listing.findOne({ name: 'LA' }))
+    .then(listing => {
+      let key = listing.id;
+      return Reservation.sync().then(() =>
+        Reservation.create({
+          checkIn: '2019-11-25',
+          totalNights: 2,
+          listingId: key
+        })
+      );
+    })
+    .then(() =>
+      Reservation.findAll({
+        where: db.where(db.fn('month', db.col('checkIn')), '11')
+      })
+    )
+    .then(data => {
+      data.map(ele => {
+        console.log(ele.toJSON());
+      });
+    })
+    .catch(err => {
+      console.error(err);
+    });
+};
+
+createRecords();

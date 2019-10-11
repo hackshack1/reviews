e7cef5f7c15b4115a165adeb118eb89d4d395784
Jimmy Ma <jimmyma@Jimmys-MacBook-Pro.local>
2019-10-11 16:50:31 -0700

    finished mongoDB review schema

---
+++ Reviews/client/src/components/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import Axios from 'Axios';
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h1>Reviews</h1>
+
+      </div>
+    )
+  }
+}
+
+export default App;

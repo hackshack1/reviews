7fa70826917e8864fe2d38cfdc9b65e855e70991
bacai dong <dongb909@gmail.com>
2019-10-18 17:40:04 -0700

    creates window of three listings

--- Recommendations/client/src/HomesRecommendation.jsx
+++ Recommendations/client/src/HomesRecommendation.jsx
@@ -3,21 +3,63 @@ import axios from 'axios';
 import Listings from './Listings.jsx';
 import styled from 'styled-components';
 
-const Wrapper = styled.div`
+const MainWrapper = styled.div`
   font-family: Helvetica Neue, sans-serif;
-  position: relative;
-  width: 1050px;
-  height: 400px;
   text-align: center;
+  border: 1px solid yellow;
+  height: 420px;
 `;
-const Button = styled.button`
+const PrevDiv = styled.div`
+  position: absolute;
+
+`;
+const PrevButton = styled.button`
   animation: slide 0.5s forwards; 
+  text-align: left;
   position: absolute;
 `;
+const Container = styled.div`
+  width: 900px;
+  height:420px;
+  border: 1px dashed grey;
+  display: inline-block;
+  overflow: hidden;
+`
+const ListingsDiv = styled.div`
+  position:relative;
+  border: 1px solid red;
+  height: 350px;
+`;
+const NextDiv = styled.div`
+position: absolute;
+
+`;
+
+const NextButton = styled.button`
+  animation: slide 0.5s forwards; 
+  position: absolute;
+  text-align: right;
+
+`;
+
+// const LeftBox = styled.div`
+//   position: absolute;
+//   width: 300px;
+//   height: 300px;
+//   z-index: 3;
+// `;
+
+// const RightBox = styled.div`
+//   position: absolute;
+//   width: 300px;
+//   height: 300px;
+//   z-index: 2;
+// `;
 
 const Title = styled.h5`
-  text-align: left
-`
+  text-align: left;
+  padding-left: 200px;
+`;
 class HomesRecommendation extends React.Component {
   constructor (props) {
     super(props);
@@ -63,13 +105,21 @@ class HomesRecommendation extends React.Component {
 
   render() {
     return (
-      <Wrapper>
-        <Title>More homes you may like</Title>
-        <Button id="outterLeftArrow" onClick={() => this.prevThree()} disabled={this.state.currIndex===0}>PREVIOUS</Button>
-        <br></br>
-        <Listings allListings={this.state.allListings} currIndex={this.state.currIndex}/>
-        <Button id="outterRightArrow" onClick={()=>this.nextThree()} disabled={this.state.currIndex === this.state.allListings.length-3}>NEXT</Button>
-      </Wrapper>
+      <MainWrapper>
+          <PrevDiv className="innerdiv">
+            <PrevButton id="outterLeftArrow" onClick={() => this.prevThree()} disabled={this.state.currIndex===0}>PREVIOUS</PrevButton>
+          </PrevDiv>
+        <Container>
+          <Title>More homes you may like</Title>
+          <ListingsDiv>
+
+          <Listings allListings={this.state.allListings} currIndex={this.state.currIndex}/>
+          </ListingsDiv>
+        </Container>
+          <NextDiv>   
+            <NextButton id="outterRightArrow" onClick={()=>this.nextThree()} disabled={this.state.currIndex === this.state.allListings.length-3}>NEXT</NextButton>
+          </NextDiv>
+      </MainWrapper>
     );
   }
 }
--- Recommendations/client/src/Listings.jsx
+++ Recommendations/client/src/Listings.jsx
@@ -3,20 +3,21 @@ import Listing from './Listing.jsx'
 import styled from 'styled-components';
 
 const MainSlider = styled.div`
-  position: bottom;
+  position: left;
   max-width: 113px;
-  margin: 0 auto;
+
 `
 
 const Wrapper = styled.div`
   display: flex;
   position: absolute;
-  transition: transform 300ms cubic-bezier(0.455, 0.03, 0.515, 0.955);
+  transition: transform 300ms;
+  border:2px solid green;
 `
-
+// cubic-bezier(0.455, 0.03, 0.515, 0.955);
 function Listings (props) {
   return (
-    <MainSlider>
+    <MainSlider className="inner">
       <Wrapper style={{'transform': `translateX(-${props.currIndex*(100/props.allListings.length)}%)`}}>
       {props.allListings.map(listing=> <Listing listing={listing}/>)}
       </Wrapper>

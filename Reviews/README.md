9d355fe084a27c1b2c5159246ad2e9e9695e9537
Jimmy Ma <jimmyma@Jimmys-MacBook-Pro.local>
2019-10-22 15:46:42 -0700

    Updated README

---
+++ Reviews/README.md
@@ -0,0 +1,13 @@
+npm instal
+
+//Start server
+
+npm run start
+
+//Run webpack
+
+npm run build
+
+//Seed database
+
+npm run seed-database
--- Reviews/server/index.js
+++ Reviews/server/index.js
@@ -12,6 +12,17 @@ app.use(express.json())
 
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+  });
+
+app.use(express.static(path.join(__dirname, '/../public')));
+
 app.use(
     '/air6n6/*/listing',
     express.static(path.join(__dirname, '../public')));

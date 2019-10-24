a377c39649b9ceba480942168bf8676b789bc493
hllywdnghts <harrison.obeid@icloud.com>
2019-10-24 11:29:16 -0700

    Chaged HTML ID for component add

--- PhotoGallery/client/index.jsx
+++ PhotoGallery/client/index.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PhotoGallery from './components/PhotoGallery.jsx/index.js';
 
-ReactDOM.render(<PhotoGallery />, document.getElementById('app'));
+ReactDOM.render(<PhotoGallery />, document.getElementById('PhotoGallery'));

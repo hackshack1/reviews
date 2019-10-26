6d2c65c34a50b01a1559aace73d485f68fb4fbb2
Jimmy Ma <jimmyma@Jimmys-MacBook-Pro.local>
2019-10-26 16:31:33 -0700

    optimizing speeds

--- Reviews/server/index.js
+++ Reviews/server/index.js
@@ -2,15 +2,38 @@ const express = require('express');
 const path = require('path');
 const controller = require('./controllers/controller.js')
 const db = require('../database/connection.js');
+const compression = require('compression')
+
 
 const bodyParser = require('body-parser')
 
 const PORT = 3004
 
 const app = express()
+
 app.use(express.json())
 
 app.use(bodyParser.json());
+
+app.use(compression())
+
+// server-sent event stream
+app.get('/events', function (req, res) {
+  res.setHeader('Content-Type', 'text/event-stream')
+  res.setHeader('Cache-Control', 'no-cache')
+
+  // send a ping approx every 2 seconds
+  var timer = setInterval(function () {
+    res.write('data: ping\n\n')
+
+    // !!! this is the important part
+    res.flush()
+  }, 2000)
+
+  res.on('close', function () {
+    clearInterval(timer)
+  })
+})
 
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -34,6 +57,8 @@ app.get('/reviews',(req, res)=>{
 app.get('*', (req, res) => {
 res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
+
+
 
 
 app.listen(PORT, ()=> console.log(`Review app listening on port ${PORT}`))
